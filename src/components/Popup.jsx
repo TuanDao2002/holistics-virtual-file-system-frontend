53e202e09019458c67bf9031a44911cf326f344d
TuanDao2002 <tuan09092002@gmail.com>
2023-11-08 13:04:23 +0700

    update popup interface

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -5,6 +5,7 @@ const Popup = (props) => {
         <div className="popup-box">
           <span className="close-icon" onClick={props.handleClose}>x</span>
           <div className="box">
+            {<h1 className="text-center text-3xl" >Instruction</h1>}
             {props.content}
           </div>
         </div>
